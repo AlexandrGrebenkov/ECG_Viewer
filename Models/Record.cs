314e799af171d623a7082bc95e86fffd2607694d
Alexandr <alexandrgrebenkov@gmail.com>
2018-09-05 16:30:47 +0500

    Класс FileWorker. Реализовано сохранение и загрузка бинарных файлов

---
+++ Models/Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECG_Viewer.Models
+{
+    public class Record
+    {
+        public List<double> Ch1;
+        public List<double> Ch2;
+    }
+}
