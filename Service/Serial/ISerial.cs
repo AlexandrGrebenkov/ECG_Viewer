c3c57deedf5489f934be508ff8d60b34e24bf779
Alexandr <alexandrgrebenkov@gmail.com>
2018-09-06 12:07:55 +0500

    Serial

---
+++ Service/Serial/ISerial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECG_Viewer.Service.Serial
+{
+    public interface ISerial
+    {
+        bool IsConnected { get; }
+        IEnumerable<string> AvailablePorts { get; }
+
+        void Connect(string portName, int baudRate, Action<string> ErrorHandler = null);
+        void Disconnect(Action<string> ErrorHandler = null);
+    }
+}
---
+++ Service/Serial/Serial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace ECG_Viewer.Service.Serial
+{
+    public class Serial : ISerial
+    {
+        SerialPort Port = new SerialPort();
+
+        public bool IsConnected => Port.IsOpen;
+
+        public IEnumerable<string> AvailablePorts => SerialPort.GetPortNames();
+
+        public void Connect(string portName, int baudRate, Action<string> ErrorHandler = null)
+        {
+            Port.PortName = portName;
+            Port.BaudRate = baudRate;
+            try
+            {
+                Port.Open();
+            }
+            catch (UnauthorizedAccessException error)
+            {
+                ErrorHandler?.Invoke("Выбранный порт занят другим приложением");
+            }
+            catch (Exception error)
+            {
+                ErrorHandler?.Invoke(error.Message);
+            }
+        }
+
+        public void Disconnect(Action<string> ErrorHandler = null)
+        {
+            try
+            {
+                Port.Close();
+            }
+            catch (Exception error)
+            {
+                ErrorHandler?.Invoke(error.Message);
+            }
+        }
+    }
+}
